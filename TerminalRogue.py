4b12bc78f651aa221750e81dd0876c38c3d7015c
Sean Scott <sean@scottlabs.org>
2024-08-15 17:30:55 -0400

    first commit

---
+++ TerminalRogue.py
@@ -0,0 +1,237 @@
+from art import *
+from random import randrange
+import time
+
+
+def spacer(count):
+    for x in range(count):
+        print()
+
+
+tprint("Term Rogue")
+spacer(2)
+print("You woke to the suffocating scent of wet earth and rot, bruises")
+print("throbbing beneath layers of grime. The trees huddle close around,")
+print("their gnarled branches twisting like skeletal fingers, blocking")
+print(" out the daylight in a sinister embrace")
+print()
+print("                              ...                                ")
+print()
+
+print()
+time.sleep(7)
+
+
+print("You had no memory of how you got there, only chilling certainty")
+print("that something was watching you from the shadows just beyond the")
+print("clearing.")
+print("                              ...                                ")
+spacer(3)
+time.sleep(4)
+
+
+print("From the shadows, a figure emerged - an old man with wild silver")
+print("hair, and eyes that seemed to hold the secrets of the forest. He")
+print("stepped closer, his voice a low, gravelly whisper.")
+spacer(2)
+print("MYSTERIOUS OLD MAN: \"Do you remember your name?\"")
+spacer(2)
+
+player_name = input("> Enter your name: ")
+
+
+print("                              ...                                ")
+
+print()
+
+print("MYSTERIOUS OLD MAN: Well " + player_name + ", I know you're probably")
+print("wondering how you got here, but the details of your arrival matter not;")
+print("what lies ahead is a battle unlike any you've known...")
+
+print()
+print("                              ...                                ")
+spacer(2)
+time.sleep(5)
+
+
+print("The old man raises his hand, and the forest erupts with a sudden")
+print("blaze of light, revealing three weapon chests nestled among the")
+print("ancient trees")
+
+spacer(2)
+
+print("[1] Red chest")
+print("[2] Blue chest")
+print("[3] Green chest")
+
+print()
+
+
+while True:
+    try:
+        chest_choice = int(input("> Select a chest: "))
+        if 1 <= chest_choice <= 3:
+            break
+        raise Exception()
+    except:
+        print("Invalid option")
+print()
+print()
+print("You open the chest and strap on the weathered defense, its leather straps")
+print("creaking with age, the metal plates cool and heavy against your bruised")
+print("skin.") 
+print()
+print("As you adjusted the final piece, your gaze caught a faint glimmer")
+print("in the shadows—a small tunnel near the chests, half-hidden by the tangled")
+print("roots, its entrance dark and foreboding")
+
+print()
+print("                              ...                                ")
+spacer(2)
+time.sleep(8)
+
+print()
+
+
+print("As you took your first step into the tunnel, the old man's laughter echoed")
+print("through the clearing - a low, sinister sound that sent a shiver down your")
+print("spine.")
+print()
+print("The moment you crossed the threshold, the tree vines slithered to life,")
+print("twisting and writhing untl they sealed the entrance behind you, plunging the")
+print("tunnel into darkness")
+print()
+
+time.sleep(5)
+
+print()
+print("                              ...                                ")
+
+spacer(2)
+print("As you attempt to get a grasp on your surroundings, a 2-headed Goblin confronts")
+print("you with its weapon drawn!")
+print()
+
+class Character:
+    def __init__(self, name, health, attack_power, defense, base_defense):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+        self.defense = defense
+        self.base_defense = base_defense # Defense level before any buffs of debuffs
+
+    def take_damage(self, damage):
+        # Defense reduces damage
+        reduced_damage = max(0, damage - self.defense)
+        self.health -= reduced_damage
+        print(f"{self.name} takes {reduced_damage} damage! HP remaining: {self.health}")
+
+
+    def defend(self, target):
+        # Boosts the defense value of target. 
+        def_boost = 5
+        self.defense += def_boost
+        print(f"{self.name} feels their armor thicken. Their defense is raised by", def_boost)
+
+
+    def rebase_def(self, target):
+        # Brings the target's defense back to its base value
+        self.defense = self.base_defense
+        print(f"{target.name} defense has returned to normal")
+
+
+    def heal(self, target):
+        heal_amount = randrange(25, 40)
+        self.health = min(100, self.health + 50) 
+        print(f"{self.name} heals for {heal_amount}. Their HP is now {self.health}")
+
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name} for {self.attack_power} damage!")
+
+        target.take_damage(self.attack_power)
+        #target.rebase_def(target)
+
+
+
+player = Character(name ="Sean", health=100, attack_power=20, defense=5, base_defense=5)
+enemy = Character(name="Goblin", health=100, attack_power=15, defense=5, base_defense=5)
+
+
+turn = 0
+
+ 
+def player_turn():
+    print("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +")
+    print()
+    print("-->", player.name + ":", player.health, "HP,", player.defense, "DEF")
+    print()
+    print("-->", enemy.name + ":", enemy.health, "HP,", enemy.defense, "DEF")
+    print()
+    print("[1] Attack")
+    print("[2] Defend")
+    print("[3] Potion")
+    print()
+
+    turn_choice = input("> ")
+
+    print()
+
+    if turn_choice.isdigit() and 0 < int(turn_choice) < 4:
+    # Check if turn_choice is a digit before converting it to integer
+        turn_choice = int(turn_choice)
+
+        if turn_choice == 1:
+            player.attack(enemy)
+            if (enemy.defense != enemy.base_defense):
+                player.rebase_def(enemy)
+            print()
+        elif turn_choice == 2:
+            player.defend(player)
+            print()
+        elif turn_choice == 3:
+            player.heal(player)
+            print()
+
+def enemy_turn():
+    print("+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +")
+    print()
+    print(f"{enemy.name} readies their weapon...")
+    print()
+    print()
+    time.sleep(1)
+    enemy_turn_choice = (randrange(1, 3))
+    # Enemy will randomly decide which choice to take in their response
+
+    if enemy_turn_choice == 1:
+        enemy.attack(player)
+        if (player.defense != player.base_defense):
+            enemy.rebase_def(player)
+        print()
+    elif enemy_turn_choice == 2:
+        enemy.defend(enemy)
+        print()
+    elif enemy_turn_choice == 3:
+        enemy.heal(enemy)
+        print()
+
+turn = 0
+# Turn 0 is the player's turn. Turn 1 is the AI's turn. The loop below will iterate between 0 and 1
+
+while True:
+    if player.health > 0 and enemy.health > 0:
+        if turn == 0:
+            player_turn()
+            turn += 1
+        elif turn == 1:
+            enemy_turn()
+            turn -= 1
+    else:
+        if player.health > enemy.health:
+            print(player.name, "has won the battle")
+            break
+        else:
+            print(enemy.name, "has won the battle")
+            break
+
+print("Press the Enter key to exit")       
+input()
